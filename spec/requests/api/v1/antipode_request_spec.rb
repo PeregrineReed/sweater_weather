787b258b3d5afcfe1fda00cbd1d918b000b1d78b
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-15 09:15:21 -0600

    Created spec for antipode request

---
+++ spec/requests/api/v1/antipode_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Antipode API' do
+  it 'returns antipode weather for searched city' do
+
+    get '/api/v1/antipode?loc=hongkong'
+
+    anitpode = JSON.parse(response.body, symbolize_names: true)
+
+    antipode_keys = [:id, :type, :attributes, :search_location]
+    attribute_keys = [:location_name, :forecast]
+    forecast_keys = [:summary, :current_temperature]
+
+    expect(antipode.keys).to eq(antipode_keys)
+    expect(antipode[:attributes].keys).to eq(attribute_keys)
+    expect(antipode[:attributes][:forecast].keys).to eq(forecast_keys)
+  end
+end
