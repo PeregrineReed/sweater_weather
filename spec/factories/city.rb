1a84bafb1aa0361230d44a43bc3844369bef5b40
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-16 12:26:38 -0600

    created spec for user city

---
+++ spec/factories/city.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :city do
+    name { 'Gotham' }
+    state { 'DC' }
+    country { 'US' }
+    place_id { 'aksjfdfladsjfldskj' }
+    latitude { 20 }
+    longitude { 80 }
+  end
+end
