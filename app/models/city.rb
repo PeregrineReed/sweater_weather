eeaefcdbf0b3b49b1ed25d7e03ae0acd817fb944
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-16 12:27:09 -0600

    updated models: user, city, user_city

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,19 +1,25 @@
-class City
-  attr_reader :name,
-              :state,
-              :country,
-              :id,
-              :latitude,
-              :longitude
+class City < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :state
+  validates_presence_of :country
+  validates_presence_of :latitude
+  validates_presence_of :longitude
+  validates :place_id, uniqueness: true, presence: true
 
-  def initialize(data)
+  has_many :user_cities
+  has_many :users, through: :user_cities
+
+  def self.create_from_hash(data)
     titles = data[:results][0][:address_components]
-    @name = titles[0][:long_name]
-    @state = titles[2][:short_name]
-    @country = titles[3][:short_name]
-    @id = data[:results][0][:place_id]
     coordinates = data[:results][0][:geometry][:location]
-    @latitude = coordinates[:lat]
-    @longitude = coordinates[:lng]
+    attributes = {
+      name: titles[0][:long_name],
+      state: titles[2][:short_name],
+      country: titles[3][:short_name],
+      place_id: data[:results][0][:place_id],
+      latitude: coordinates[:lat],
+      longitude: coordinates[:lng]
+    }
+    City.create(attributes)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   validates_presence_of :password
   validates_uniqueness_of :api_key
 
+  has_many :user_cities
+  has_many :cities, through: :user_cities
+
   def self.create_if_valid(params)
     user = User.new(params)
     if user.save && user.update_attribute(:api_key, SecureRandom.urlsafe_base64)
