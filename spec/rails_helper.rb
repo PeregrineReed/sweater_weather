43dba09d5a450f0b0242b6d09f8c6a1c659e02f0
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-15 21:26:21 -0600

    Added bcrypt and factory bot + config for factories

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'pry'
 require 'simplecov'
+require './spec/support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
 SimpleCov.start 'rails'
 
