af8276f1ecb68fe79e14367c7f8cc5286c13a96f
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-15 22:21:57 -0600

    Updated base and user controllers and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password
   validates_uniqueness_of :api_key
+
+  def self.create_if_valid(params)
+    user = User.new(params)
+    if user.save && user.update_attribute(:api_key, SecureRandom.urlsafe_base64)
+      user
+    end
+  end
 end
