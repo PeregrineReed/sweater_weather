6c00090cbfdc19b8a60ea307cb3e77d5c7c9773c
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-15 22:51:25 -0600

    updated session request spec

--- spec/requests/api/v1/session_request_spec.rb
+++ spec/requests/api/v1/session_request_spec.rb
@@ -12,11 +12,25 @@ describe 'Session API' do
       password: 'password'
     }
 
-    post '/api/v1/sessions', params: { user: credentials }
+    post '/api/v1/sessions', params: { session: credentials }
     results = JSON.parse(response.body, symbolize_names: true)
 
     expect(results).to eq({
       api_key: "WN_7kope0QzswDKpdGGd7Q"
+      })
+  end
+
+  it 'returns an error if a user doesn\'t exist with those credentials' do
+    credentials = {
+      email: 'user@email.com',
+      password: 'password'
+    }
+
+    post '/api/v1/sessions', params: { session: credentials }
+    results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(results).to eq({
+      error: "Invalid email or password."
       })
   end
 end
