cfc365127c2073df605f57ecbd214d1db63747b0
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-16 12:24:51 -0600

    updated favorites controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,10 +1,22 @@
 class Api::V1::FavoritesController < Api::V1::BaseController
   def create
+    user = User.find_by(api_key: favorite_params[:api_key])
+    city = City.find_by(place_id: favorite_params[:location])
+    user.cities << city
+    head 201
   end
 
   def index
+    user = User.find_by(api_key: favorite_params[:api_key])
+    render json: user.cities
   end
 
   def destroy
+  end
+
+  private
+
+  def favorite_params
+    params.permit(:api_key, :location)
   end
 end
