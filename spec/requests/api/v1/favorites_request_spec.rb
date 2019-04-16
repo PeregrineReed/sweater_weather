ce6f938544e3efe98feee548deb3db12a2c398e2
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-16 12:24:13 -0600

    Updated favorite request spec

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 describe 'Favorites API' do
   it 'user can add a favorited city' do
     user = create(:user, api_key: 'jgn983hy48thw9begh98h4539h4')
-    create(:city, id: 'ChIJzxcfI6qAa4cR1jaKJ_j0jhE')
+    city = create(:city, place_id: 'ChIJzxcfI6qAa4cR1jaKJ_j0jhE')
+
     post '/api/v1/favorites', params: {
-      favorite: {
-        location: 'ChIJzxcfI6qAa4cR1jaKJ_j0jhE',
-        api_key: 'jgn983hy48thw9begh98h4539h4'
-        }
-      }
+      location: city.place_id,
+      api_key: user.api_key
+    }.to_json, headers: {
+      "Content-Type": "application/json",
+       Accept: "application/json"
+    }
 
     expect(response.status).to eq(201)
     expect(user.cities.count).to eq(1)
@@ -17,22 +19,25 @@ describe 'Favorites API' do
 
   it 'user can list their favorite cities' do
     user = create(:user, api_key: 'jgn983hy48thw9begh98h4539h4')
-    create(:city, id: 'ChIJzxcfI6qAa4cR1jaKJ_j0jhE', name: 'Denver')
-    create(:city, id: 'ChIJdd4hrwug2EcRmSrV3Vo6llI', name: 'London')
-    create(:city, id: 'ChIJ5TCOcRaYpBIRCmZHTz37sEQ', name: 'Barcelona')
+    create(:city, place_id: 'ChIJzxcfI6qAa4cR1jaKJ_j0jhE', name: 'Denver')
+    create(:city, place_id: 'ChIJdd4hrwug2EcRmSrV3Vo6llI', name: 'London')
+    create(:city, place_id: 'ChIJ5TCOcRaYpBIRCmZHTz37sEQ', name: 'Barcelona')
 
     get '/api/v1/favorites', params: {
-      favorite: {
-        api_key: 'jgn983hy48thw9begh98h4539h4'
-        }
-      }
-
+      api_key: 'jgn983hy48thw9begh98h4539h4'
+    }
+    results = JSON.parse(response.body, symbolize_names: true)
+    favorites_keys = [:id, :type, :attributes]
+    attribute_keys = [:place_id, :name, :state, :country, :latitude, :longitude]
+    expect(results[:data][:type]).to eq(:city)
+    expect(results[:data].keys).to eq(favorites_keys)
+    expect(results[:data][:attributes].keys).to eq(attribute_keys)
   end
   it 'user can delete a favorited city' do
     user = create(:user, api_key: 'jgn983hy48thw9begh98h4539h4')
-    create(:city, id: 'ChIJzxcfI6qAa4cR1jaKJ_j0jhE', name: 'Denver')
-    create(:city, id: 'ChIJdd4hrwug2EcRmSrV3Vo6llI', name: 'London')
-    create(:city, id: 'ChIJ5TCOcRaYpBIRCmZHTz37sEQ', name: 'Barcelona')
+    create(:city, place_id: 'ChIJzxcfI6qAa4cR1jaKJ_j0jhE', name: 'Denver')
+    create(:city, place_id: 'ChIJdd4hrwug2EcRmSrV3Vo6llI', name: 'London')
+    create(:city, place_id: 'ChIJ5TCOcRaYpBIRCmZHTz37sEQ', name: 'Barcelona')
 
     delete '/api/v1/favorites', params: {
       favorite: {
