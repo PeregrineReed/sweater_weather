7362de6b4cdbeca3c77acaa897b4f1fecae224de
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-16 22:57:17 -0600

    Refactored forecast model

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -13,34 +13,40 @@ class Forecast
               :visibility,
               :uv_index,
               :cloud_cover,
-              :hourly,
-              :daily,
               :high,
               :low
 
-  def initialize(city, forecast)
+  def initialize(city, weather)
     @id = city.place_id
     @city = city.name
     @state = city.state
     @country = city.country
-    @timezone = forecast[:timezone]
-    @time = forecast[:currently][:time]
-    @icon = forecast[:currently][:icon]
-    @summary = forecast[:currently][:summary]
-    @temperature = forecast[:currently][:temperature]
-    @feels_like = forecast[:currently][:apparentTemperature]
-    @humidity = forecast[:currently][:humidity]
-    @visibility = forecast[:currently][:visibility]
-    @uv_index = forecast[:currently][:uvIndex]
-    @cloud_cover = forecast[:currently][:cloudCover]
-    @hourly = forecast[:hourly][:data].map do |hour|
+    @timezone = weather[:timezone]
+    @time = weather[:currently][:time]
+    @icon = weather[:currently][:icon]
+    @summary = weather[:currently][:summary]
+    @temperature = weather[:currently][:temperature]
+    @feels_like = weather[:currently][:apparentTemperature]
+    @humidity = weather[:currently][:humidity]
+    @visibility = weather[:currently][:visibility]
+    @uv_index = weather[:currently][:uvIndex]
+    @cloud_cover = weather[:currently][:cloudCover]
+    @hourly = weather[:hourly][:data]
+    @daily = weather[:daily][:data]
+    @high = weather[:daily][:data][0][:temperatureMax]
+    @low = weather[:daily][:data][0][:temperatureMin]
+  end
+
+  def hourly
+    @hourly[0..7].map do |hour|
       Hour.new(@id, hour)
     end
-    @daily = forecast[:daily][:data].map do |day|
+  end
+
+  def daily
+    @daily[0..6].map do |day|
       Day.new(@id, day)
     end
-    @high = @daily[0].high
-    @low = @daily[0].low
   end
 
   def cache_key
