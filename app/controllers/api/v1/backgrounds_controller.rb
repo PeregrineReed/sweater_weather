5bc8e4fff8fad6875e03cbd81ba86625b84f2fa0
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-16 22:06:48 -0600

    Updated controllers after removing background facade

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BackgroundsController < Api::V1::BaseController
   def show
-    @background = BackgroundsFacade.new(params[:location]).image
+    @background = Background.new(params[:location])
     render json: BackgroundSerializer.new(@background)
   end
 end
