ce7aca6520dd2f823d4dcb7ce5b4f49c5fe53a54
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-16 22:04:19 -0600

    Updated serializers for caching

--- app/serializers/hourly_serializer.rb
+++ app/serializers/hourly_serializer.rb
@@ -1,5 +1,8 @@
 class HourlySerializer
   include FastJsonapi::ObjectSerializer
+  cache_options enabled: true,
+  cache_length: 1.hour
+  
   attributes :id,
              :time,
              :temperature,
